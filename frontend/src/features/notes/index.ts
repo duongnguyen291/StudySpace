02d0442e386d6bc8d4866f0f2e69c4bb66bc1e95
kimcuong04 <nhi.nl225991@sis.hust.edu.vm>
2025-12-04 20:40:32 +0700

    feature(notes-cate): update category

--- frontend/src/features/notes/index.ts
+++ frontend/src/features/notes/index.ts
@@ -8,12 +8,16 @@ export { QuickNotePopup } from './components/QuickNotePopup'
 export { QuickNoteFloatingButton } from './components/QuickNoteFloatingButton'
 export { ExportButton } from './components/ExportButton'
 export { RichTextEditor } from './components/RichTextEditor'
+export { NoteCategorySelector } from './components/NoteCategorySelector'
 
 // Hooks
 export { useQuickNote } from './hooks/useQuickNote'
 
+// Services
+export { noteService, noteCategoryService } from './services/noteService'
+
 // Types
-export type { Note, NoteCreate, NoteUpdate } from './types/note.types'
+export type { Note, NoteCreate, NoteUpdate, NoteCategory, NoteCategoryCreate, NoteCategoryUpdate } from './types/note.types'
 
 // Utils
 export { exportToDocx, exportToPdf, exportToTxt, exportToHtml } from './utils/exportNote'
